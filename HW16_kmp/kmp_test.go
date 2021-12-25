9656392c73c597ff69adbc207a10b5ecc12fae00
Alexander Borodin <avbruu@gmail.com>
2021-12-25 14:51:03 +0300

    rename test

--- HW16_kmp/kmp_test.go
+++ HW16_kmp/kmp_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestCreatePiSlow(t *testing.T) {
+func TestCreatePi(t *testing.T) {
 	s := "AABAABAAA"
 	pi := []int{0, 0, 1, 0, 1, 2, 3, 4, 5, 2}
 	require.Equal(t, pi, CreatePiSlow(s))
@@ -17,6 +17,7 @@ func TestCreatePiSlow(t *testing.T) {
 func TestKMP(t *testing.T) {
 	require.Equal(t, []int(nil), KMP("AABBBBAA", "C"))
 	require.Equal(t, []int{0, 1, 2, 3, 4, 5}, KMP("AAAAAA", "A"))
+	require.Equal(t, []int{0, 1, 2, 3}, KMP("AAAAA", "AA"))
 	require.Equal(t, []int{0, 6}, KMP("AABBBBAA", "AA"))
 	require.Equal(t, []int{0, 7}, KMP("AABABABAAB", "AAB"))
 }
