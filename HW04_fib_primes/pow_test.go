98d7b5a29b4efb7f14923945e6fff429a5966c02
Alexander Borodin <avbruu@gmail.com>
2021-12-28 15:58:05 +0300

    hw04 add test spent time

--- HW04_fib_primes/pow_test.go
+++ HW04_fib_primes/pow_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"math"
 	"testing"
+	"time"
 )
 
 func TestPow(t *testing.T) {
@@ -16,10 +17,11 @@ func TestPow(t *testing.T) {
 	for reader.Next(t) {
 		num, pow := reader.In.FloatInt()
 		out := reader.Out.Float()
-
+		now := time.Now()
 		res := PowN(num, pow)
+		dur := time.Since(now)
 		delta := 0.0000001
-		fmt.Printf("case %d: want: %2f have: %2f PASS: %v\n", reader.Idx-1, out, res, (res-out) < delta)
+		fmt.Printf("case %d: want: %2f have: %2f PASS: %v spent: %s \n", reader.Idx-1, out, res, (res-out) < delta, dur)
 		require.Equal(t, math.Abs(res-out) < delta, true)
 	}
 
@@ -27,10 +29,11 @@ func TestPow(t *testing.T) {
 	for reader.Next(t) {
 		num, pow := reader.In.FloatInt()
 		out := reader.Out.Float()
-
+		now := time.Now()
 		res := PowAdd(num, pow)
+		dur := time.Since(now)
 		delta := 0.0000001
-		fmt.Printf("case %d: want: %2f have: %2f PASS: %v\n", reader.Idx-1, out, res, (res-out) < delta)
+		fmt.Printf("case %d: want: %2f have: %2f PASS: %v spent: %s\n", reader.Idx-1, out, res, (res-out) < delta, dur)
 		require.Equal(t, math.Abs(res-out) < delta, true)
 	}
 }
