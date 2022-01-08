e6409dcb9dd9503737bc8f322cd5b5240d082fac
Alexander Borodin <avbruu@gmail.com>
2022-01-08 11:15:31 +0300

    hw15 bm

--- HW15_bm/bm.go
+++ HW15_bm/bm.go
@@ -1,9 +1,85 @@
 package HW15_bm
 
-func IndexBruteForce2(s, p string) int {
+import "strings"
+
+func BM(s, p string) int {
+	if len(s) == 0 || len(p) == 0 {
+		return -1
+	}
+
+	prefix := prefixTable(p)
+	suffix := suffixTable(p)
+	i := len(p) - 1
+	for i < len(s) {
+		j := len(p) - 1
+		for j >= 0 && s[i] == p[j] {
+			i--
+			j--
+		}
+		if j < 0 {
+			return i + 1
+		}
+		i += max(prefix[s[i]], suffix[j])
+	}
+	return -1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func suffixTable(pattern string) []int {
+	m := len(pattern)
+	table := make([]int, m)
+
+	last := m - 1
+
+	lastPrefix := last
+	for i := last; i >= 0; i-- {
+		if strings.HasPrefix(pattern, pattern[i+1:]) {
+			lastPrefix = i + 1
+		}
+
+		table[i] = lastPrefix + last - i
+	}
+
+	for i := 0; i < last; i++ {
+		lenSuffix := suffixLength(pattern, pattern[1:i+1])
+		if pattern[i-lenSuffix] != pattern[last-lenSuffix] {
+			table[last-lenSuffix] = lenSuffix + last - i
+		}
+	}
+
+	return table
+}
+
+func prefixTable(p string) [256]int {
+	var t [256]int
+	for i := range t {
+		t[i] = len(p)
+	}
+
+	for i := 0; i < len(p)-1; i++ {
+		t[p[i]] = len(p) - 1 - i
+	}
+	return t
+}
+
+func suffixLength(a, b string) (i int) {
+	for ; i < len(a) && i < len(b); i++ {
+		if a[len(a)-1-i] != b[len(b)-1-i] {
+			break
+		}
+	}
+	return
+}
+
+func IndexBruteForce(s, p string) int {
 	for is := 0; is <= len(s)-len(p); is++ {
 		for ip := 0; ip < len(p); ip++ {
-			//fmt.Printf("%d %d %s %s \n", is, ip, string(s[is+ip]), string(p[ip]))
 			if s[is+ip] != p[ip] {
 				break
 			}
@@ -15,4 +91,3 @@ func IndexBruteForce2(s, p string) int {
 
 	return -1
 }
-
--- HW15_bm/bm_test.go
+++ HW15_bm/bm_test.go
@@ -1,15 +1,27 @@
 package HW15_bm
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
+func TestBM(t *testing.T) {
+	require.Equal(t, -1, BM("", ""))
+	require.Equal(t, -1, BM("AA", "B"))
+	require.Equal(t, 0, BM("A", "A"))
+	require.Equal(t, 1, BM("BA", "A"))
+	require.Equal(t, 0, BM("ABCD", "ABCD"))
+	require.Equal(t, 5, BM("AAAAAABCD", "ABCD"))
+
+	fmt.Println(suffixTable("abcababc"))
+}
+
 func TestIndexBruteForce(t *testing.T) {
-	require.Equal(t, -1, IndexBruteForce2("", ""))
-	require.Equal(t, -1, IndexBruteForce2("AA", "B"))
-	require.Equal(t, 0, IndexBruteForce2("A", "A"))
-	require.Equal(t, 1, IndexBruteForce2("BA", "A"))
-	require.Equal(t, 0, IndexBruteForce2("ABCD", "ABCD"))
-	require.Equal(t, 5, IndexBruteForce2("AAAAAABCD", "ABCD"))
+	require.Equal(t, -1, IndexBruteForce("", ""))
+	require.Equal(t, -1, IndexBruteForce("AA", "B"))
+	require.Equal(t, 0, IndexBruteForce("A", "A"))
+	require.Equal(t, 1, IndexBruteForce("BA", "A"))
+	require.Equal(t, 0, IndexBruteForce("ABCD", "ABCD"))
+	require.Equal(t, 5, IndexBruteForce("AAAAAABCD", "ABCD"))
 }
